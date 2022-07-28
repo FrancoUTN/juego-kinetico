31299be2ddd2d7a074427f53a34dbce0b8b058ce
unknown <cataniafrancoar@gmail.com>
2022-07-28 12:43:09 -0300

    Iconos

--- src/constants/styles.js
+++ src/constants/styles.js
@@ -2,7 +2,6 @@ export const Colors = {
   primary100: '#397367',
   primary500: '#0D1821',
   primary800: '#0079EF',
-  secondary1: '#F9C80E',
   error100: '#F68E91',
-  error500: '#ED1D24',
+  error500: '#ED1D24'
 }
