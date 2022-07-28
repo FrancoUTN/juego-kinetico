7fd0c7a931b36cbbc4d50308e69b7a3afd19181a
unknown <cataniafrancoar@gmail.com>
2022-07-28 01:59:55 -0300

    Primera aproximación

--- src/screens/PrincipalScreen.js
+++ src/screens/PrincipalScreen.js
@@ -3,29 +3,34 @@ import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { Accelerometer } from 'expo-sensors';
 
 export default function App() {
-  const [data, setData] = useState({
-    x: 0,
-    y: 0,
-    z: 0,
-  });
+  // const [data, setData] = useState({
+  //   x: 0,
+  //   y: 0,
+  //   z: 0,
+  // });
   const [subscription, setSubscription] = useState(null);
   const [valorDeTop, setValorDeTop] = useState(0);
 
   const _slow = () => {
-    Accelerometer.setUpdateInterval(1000);
+    Accelerometer.setUpdateInterval(800);
   };
 
   const _fast = () => {
-    Accelerometer.setUpdateInterval(500);
+    Accelerometer.setUpdateInterval(50);
   };
 
   const _subscribe = () => {
-    Accelerometer.setUpdateInterval(1000);
+    // Accelerometer.setUpdateInterval(100);
     setSubscription(
       Accelerometer.addListener(accelerometerData => {
-        console.log(accelerometerData);
-        setData(accelerometerData);
-        setValorDeTop(valorPrevio => valorPrevio + 1)
+        // console.log(accelerometerData);
+        // setData(accelerometerData);
+        if (accelerometerData.y > 0) {
+          setValorDeTop(valorPrevio => valorPrevio + 2)
+        }
+        else if (accelerometerData.y < 0) {
+          setValorDeTop(valorPrevio => valorPrevio - 2)
+        }
       })
     );
   };
@@ -44,15 +49,19 @@ export default function App() {
     top: valorDeTop
   };
 
-  const { x, y, z } = data;
+  function reiniciar() {
+    setValorDeTop(0);
+  }
+
+  // const { x, y, z } = data;
   return (
     <View style={styles.container}>
 
       <View style={styles.datosContainer}>
-        <Text style={styles.text}>Accelerometer: (in Gs where 1 G = 9.81 m s^-2)</Text>
+        {/* <Text style={styles.text}>Accelerometer: (in Gs where 1 G = 9.81 m s^-2)</Text>
         <Text style={styles.text}>
           x: {round(x)} y: {round(y)} z: {round(z)}
-        </Text>
+        </Text> */}
         <View style={styles.buttonContainer}>
           <TouchableOpacity onPress={subscription ? _unsubscribe : _subscribe} style={styles.button}>
             <Text>{subscription ? 'On' : 'Off'}</Text>
@@ -62,6 +71,9 @@ export default function App() {
           </TouchableOpacity>
           <TouchableOpacity onPress={_fast} style={styles.button}>
             <Text>Fast</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={reiniciar} style={styles.button}>
+            <Text>Restart</Text>
           </TouchableOpacity>
         </View>
       </View>
@@ -86,6 +98,8 @@ function round(n) {
   // return n;
 }
 
+const lado = 15;
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -107,14 +121,14 @@ const styles = StyleSheet.create({
   },
   cuadrado: {
     backgroundColor: '#eee',
-    width: 50,
-    height: 50,
+    width: lado,
+    height: lado,
     // top: 60
   },
   otroCuadrado: {
     backgroundColor: '#ada',
-    width: 50,
-    height: 50,
+    width: lado,
+    height: lado,
     position: 'absolute',
   },
   text: {
