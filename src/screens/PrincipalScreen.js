5b755c143c21c7063f5a02aba7d747798e2156ee
unknown <cataniafrancoar@gmail.com>
2022-07-28 01:29:13 -0300

    Estilos de prueba

--- src/screens/PrincipalScreen.js
+++ src/screens/PrincipalScreen.js
@@ -47,27 +47,32 @@ export default function App() {
   const { x, y, z } = data;
   return (
     <View style={styles.container}>
-      <View style={styles.otroCuadrado}>
 
+      <View style={styles.datosContainer}>
+        <Text style={styles.text}>Accelerometer: (in Gs where 1 G = 9.81 m s^-2)</Text>
+        <Text style={styles.text}>
+          x: {round(x)} y: {round(y)} z: {round(z)}
+        </Text>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity onPress={subscription ? _unsubscribe : _subscribe} style={styles.button}>
+            <Text>{subscription ? 'On' : 'Off'}</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={_slow} style={[styles.button, styles.middleButton]}>
+            <Text>Slow</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={_fast} style={styles.button}>
+            <Text>Fast</Text>
+          </TouchableOpacity>
+        </View>
       </View>
-      <View style={[styles.cuadrado, otrosEstilos]}>
 
+      <View style={styles.cuadradoContainer}>
+        <View style={styles.otroCuadrado}>
+        </View>
+        <View style={[styles.cuadrado, otrosEstilos]}>
+        </View>
       </View>
-      {/* <Text style={styles.text}>Accelerometer: (in Gs where 1 G = 9.81 m s^-2)</Text>
-      <Text style={styles.text}>
-        x: {round(x)} y: {round(y)} z: {round(z)}
-      </Text>
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity onPress={subscription ? _unsubscribe : _subscribe} style={styles.button}>
-          <Text>{subscription ? 'On' : 'Off'}</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={_slow} style={[styles.button, styles.middleButton]}>
-          <Text>Slow</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={_fast} style={styles.button}>
-          <Text>Fast</Text>
-        </TouchableOpacity>
-      </View> */}
+
     </View>
   );
 }
@@ -85,7 +90,20 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
+    // alignItems: 'center'
+  },
+  datosContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20
+  },
+  cuadradoContainer: {
+    flex: 4,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#a0a',
+    // alignItems: 'center'
   },
   cuadrado: {
     backgroundColor: '#eee',
